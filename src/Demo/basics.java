ae841566a83707ff72808f52aee6b6f26046a869
TKFisher <tracy.k.fisher@gmail.com>
2017-03-30 16:21:30 -0700

    Add TestNG test

--- src/Demo/basics.java
+++ src/Demo/basics.java
@@ -3,6 +3,7 @@ package Demo;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -10,10 +11,12 @@ import static org.hamcrest.core.IsEqual.equalTo;
 
 public class basics {
 
-    public static void main(String[] args) {
+    //public static void main(String[] args) {
 
-        // Pass Base URL
-        RestAssured.baseURI = "https://maps.googleapis.com/";
+        @Test
+        public void Test () {
+            // Pass Base URL
+            RestAssured.baseURI = "https://maps.googleapis.com/";
 
         /*  Pass Header, params, cookies
             given()
@@ -29,18 +32,19 @@ public class basics {
         *   State asserts
             then()
                 assertThat()  */
-
-        given().
-                param("location", "-33.8670522,151.195736").
-                param("radius", "500").
-                param("key", "AIzaSyBde6fW-IAx1j-J5TqNOwmvx-_QPHozqRY").
-                when().
-                get("/maps/api/place/nearbysearch/json").
-                then().assertThat().statusCode(200).and().contentType(ContentType.JSON).
-                and().body("results[0].name", equalTo("Sydney"));
-        ;
-
+            
+            given().
+                    param("location", "-33.8670522,151.195736").
+                    param("radius", "500").
+                    param("key", "AIzaSyBde6fW-IAx1j-J5TqNOwmvx-_QPHozqRY").
+                    when().
+                    get("/maps/api/place/nearbysearch/json").
+                    then().assertThat().statusCode(200).and().
+                    contentType(ContentType.JSON).and().
+                    body("results[0].name", equalTo("Sydney")).and().
+                    body("results[0].place_id", equalTo("ChIJP3Sa8ziYEmsRUKgyFmh9AQM"));
+        }
     }
 
 
-}
+
