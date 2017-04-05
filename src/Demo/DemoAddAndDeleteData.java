05eacd6a26eb403ff631f22ec5c5c29a3bcd9a91
TKFisher <tracy.k.fisher@gmail.com>
2017-04-05 14:47:19 -0700

    Add body class and method and removed hard coded value from main class

--- src/Demo/DemoAddAndDeleteData.java
+++ src/Demo/DemoAddAndDeleteData.java
@@ -15,6 +15,7 @@ import java.util.Properties;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.core.IsEqual.equalTo;
 import files.resource;
+import files.body;
 
 public class DemoAddAndDeleteData {
 
@@ -30,27 +31,14 @@ public class DemoAddAndDeleteData {
 @Test
     public void addAndDeletePlace(){
 
-    String body =
-            "{"+
-            "\"location\": {"+
-            "\"lat\": -33.8669710,"+
-            "\"lng\": 151.1958750"+
-            "},"+
-            "\"accuracy\": 50,"+
-            "\"name\": \"Google Shoes!\","+
-            "\"phone_number\": \"(02) 9374 4000\","+
-            "\"address\": \"48 Pirrama Road, Pyrmont, NSW 2009, Australia\","+
-            "\"types\": [\"shoe_store\"],"+
-            "\"website\": \"http://www.google.com.au/\","+
-            "\"language\": \"en-AU\""+
-            "}";
+    String b;
 
     RestAssured.baseURI = properties.getProperty("HOST");
 
     // Get the response
     Response response = given().
             queryParam("key", properties.getProperty("KEY")).
-            body(body).
+            body(body.getPostData()).
             when().
             post(resource.placePostData()).
             then().assertThat().statusCode(200).and().
